c53e5d3bd57ee60e0f8231bd33f8307f6a5fcc14
Sergey Krushinsky <krushinsky@gmail.com>
2019-11-24 23:26:42 +0300

    genres list mockup

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ function make_dirs(done) {
 	    return gulp.src('*.*', {read: false})
 	        .pipe(gulp.dest('./dist/css'))
 	        .pipe(gulp.dest('./dist/img'))
-	        .pipe(gulp.dest('./dist/img/icons'))
 	        .pipe(gulp.dest('./dist/fonts'))
 	        .pipe(gulp.dest('./dist/js'));
 	});
@@ -24,13 +23,25 @@ function dist_sources(done) {
 	done();
 }
 
-function dist_uikit(done) {
-	gulp.src('./node_modules/uikit/dist/css/uikit.min.css')
+function dist_framework(done) {
+	gulp.src('./node_modules/materialize-css/dist/css/materialize.min.css')
 		.pipe(gulp.dest('./dist/css'));
-	gulp.src('./node_modules/uikit/dist/js/uikit.min.js')
+	gulp.src('./node_modules/materialize-css/dist/js/materialize.min.js')
 		.pipe(gulp.dest('./dist/js'));
-	gulp.src('./node_modules/uikit/dist/js/uikit-icons.min.js')
-		.pipe(gulp.dest('./dist/js'));
+	done();
+}
+
+function dist_fonts(done) {
+	gulp.src('./node_modules/material-design-icons/iconfont/**/*')
+		.pipe(gulp.dest('./dist/fonts/iconfont'));
+	gulp.src('./node_modules/roboto-fontface/css/roboto/roboto-fontface.css')
+		.pipe(gulp.dest('./dist/css/roboto'));
+	gulp.src('./node_modules/roboto-fontface/css/roboto-condensed/roboto-condensed-fontface.css')
+		.pipe(gulp.dest('./dist/css/roboto-condensed'));
+	gulp.src('./node_modules/roboto-fontface/css/roboto-slab/roboto-slab-fontface.css')
+		.pipe(gulp.dest('./dist/css/roboto-slab'));
+	gulp.src('./node_modules/roboto-fontface/fonts/**/*')
+		.pipe(gulp.dest('./dist/fonts'));
 
 	done();
 }
@@ -41,12 +52,12 @@ gulp.task('clean', done => {
   done();
 });
 
-gulp.task('build', series(make_dirs, parallel(dist_uikit, dist_sources)), done => {
+gulp.task('build', series(make_dirs, parallel(dist_framework, dist_sources, dist_fonts)), done => {
   done();
 });
 
 gulp.task('serve', series('build', () => {
-  gulp.src('./dist').pipe(server({
+  gulp.src('./dist/').pipe(server({
 			livereload: true,
 			open: true
 		}));
